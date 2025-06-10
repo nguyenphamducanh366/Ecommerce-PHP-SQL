d60b03296c0739449e95cfe9272c037b2d603aa6
nguyenphamducanh366 <nguyenphamducanh366@gmail.com>
2025-06-10 08:57:35 +0700

    thêm các trang tại phần website

---
+++ include/footer.php
@@ -0,0 +1,69 @@
+<!-- footer -->
+<footer>
+		<div class="footer-top-first">
+			<div class="container py-md-5 py-sm-4 py-3">
+				<!-- footer first section -->
+				<h2 class="footer-top-head-w3l font-weight-bold mb-2">Giới thiệu về chúng tôi :</h2>
+				<p class="footer-main mb-4">
+				Nếu bạn đang cân nhắc một chiếc máy tính xách tay mới, đang tìm kiếm một dàn âm thanh nổi trên xe hơi mới mạnh mẽ hoặc
+				 mua một HDTV mới, chúng tôi sẽ giúp bạn dễ dàng tìm thấy chính xác những gì bạn cần với mức giá bạn có thể mua được. 
+				 Chúng tôi cung cấp Giá thấp hàng ngày trên TV, máy tính xách tay, điện thoại di động, máy tính bảng và iPad, 
+				 trò chơi điện tử, máy tính để bàn, máy ảnh và máy quay phim, âm thanh, video và hơn thế nữa.</p>
+				<!-- //footer first section -->
+				<!-- footer second section -->
+				<div class="row w3l-grids-footer border-top border-bottom py-sm-4 py-3">
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-dolly"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Miễn phí ship</h3>
+								<p>Đơn hàng trên 500,000</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer my-md-0 my-4">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-shipping-fast"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Giao hàng nhanh</h3>
+								<p>Trên toàn quốc</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="far fa-thumbs-up"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Sự lựa chọn đáng tin cậy</h3>
+								<p>Đảm bảo</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="bando col mgb l-4 m-12 c-12">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5926.376356564528!2d105.71840557348725!3d21.23912400415995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134fc459a5597c7%3A0x39906dec6991e311!2zQ8O0bmcgVHkgSG9uZGEgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1611125552801!5m2!1svi!2s"
+                        width="100%" height="200px" frameborder="0" style="border:0;" allowfullscreen=""
+                        aria-hidden="false" tabindex="0"></iframe>
+                </div>
+				<!-- //footer second section -->
+			</div>
+		</div>
+		
+	</footer>
+	<!-- //footer -->
+	<!-- copyright -->
+	<div class="copy-right py-3">
+		<div class="container">
+			<p class="text-center text-white">© 2025 Electro Store. All rights reserved
+			</p>
+		</div>
+	</div>
+	<!-- //copyright -->
